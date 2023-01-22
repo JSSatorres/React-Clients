d18d045d45b06d31b2839ad38e983ba1ac743b37
jssatorres <juansataz.dev@gmail.com>
2023-01-22 12:50:39 +0100

    Fix (TodoList) clean code

--- Context-Reducer/src/components/TodoList/TodoList.jsx
+++ Context-Reducer/src/components/TodoList/TodoList.jsx
@@ -5,43 +5,41 @@ import TodoItem from "../TodoItem";
 const TodoList = () => {
   const url = 'http://localhost:5000/api/todo'
   const { data:todos, loading, error } = useFetch(url)
-
   
-  return (
-  
+  return ( 
 
-      <Droppable droppableId="todos">
-        {(droppableProvided) => (
-          <div
-            ref={droppableProvided.innerRef}
-            {...droppableProvided.droppableProps}
-            className="mt-8 overflow-hidden rounded-t-md bg-white transition-all duration-1000 dark:bg-gray-800 [&>article]:p-4"
-          >
-            {todos?.map((todo, index) => (
-              <Draggable
-                key={todo._id}
-                // drag and drop configuration Draggable
-                index={index}
-                draggableId={`${todo._id}`}
-              >
-                {(draggableProvided) => (
-                  <TodoItem
-                    todo={todo}
-                    // removeTodo={removeTodo}
-                    // updateTodo={updateTodo}
-                    // drag and drop configuration
-                    ref={draggableProvided.innerRef}
-                    {...draggableProvided.dragHandleProps}
-                    {...draggableProvided.draggableProps}
-                  />
-                )}
-              </Draggable>
-            ))}
-            {/* reserved the blank positon */}
-            {droppableProvided.placeholder}
-          </div>
+    <Droppable droppableId="todos">
+    {(droppableProvided) => (
+      <div
+        ref={droppableProvided.innerRef}
+        {...droppableProvided.droppableProps}
+        className="mt-8 overflow-hidden rounded-t-md bg-white transition-all duration-1000 dark:bg-gray-800 [&>article]:p-4"
+      >
+      {todos?.map((todo, index) => (
+        <Draggable
+          key={todo._id}
+          // drag and drop configuration Draggable
+          index={index}
+          draggableId={`${todo._id}`}
+        >
+        {(draggableProvided) => (
+          <TodoItem
+            todo={todo}
+            // removeTodo={removeTodo}
+            // updateTodo={updateTodo}
+            // drag and drop configuration
+            ref={draggableProvided.innerRef}
+            {...draggableProvided.dragHandleProps}
+            {...draggableProvided.draggableProps}
+          />
         )}
-      </Droppable>
+        </Draggable>
+      ))}
+      {/* reserved the blank positon */}
+      {droppableProvided.placeholder}
+      </div>
+    )}
+    </Droppable>
   );
 };
 
